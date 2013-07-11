272c59892bb1ec5c8f10f15f95b658b72f98c6c8
Heryandi <heryandi@gmail.com>
2013-07-11 23:55:26 +0800

    Add some documentation

--- djangopypi2/apps/pypi_ui/urls.py
+++ djangopypi2/apps/pypi_ui/urls.py
@@ -2,7 +2,9 @@ from urlparse import urljoin
 from django.conf.urls import patterns, include, url
 from django.conf import settings
 from django.http import HttpResponseRedirect
+from django.views.generic import TemplateView
 
 urlpatterns = patterns('',
     url(r'^favicon\.ico$', lambda request: HttpResponseRedirect(urljoin(settings.STATIC_URL, 'favicon.ico'))),
+    url(r'^guide/$', TemplateView.as_view(template_name="pypi_ui/guide.html"), name='djangopypi2-guide'),
 )
